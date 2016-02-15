21ac266453d84dbeb79262f8bcb441e6b0ec4476
FlashCat <FlashCat@users.noreply.github.com>
2016-02-15 20:48:36 +0000

    rebuild pages at d274541

---
+++ implementors/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['vec_map'] = ["impl&lt;V: <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a> for <a class='struct' href='vec_map/struct.VecMap.html' title='vec_map::VecMap'>VecMap</a>&lt;V&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
