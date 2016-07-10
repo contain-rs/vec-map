39453788000d7048b5df640e313aa7d476393534
FlashCat <FlashCat@users.noreply.github.com>
2016-07-10 21:24:05 +0000

    rebuild pages at 1d55e8f

--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['vec_map'] = ["impl&lt;V&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='vec_map/struct.VecMap.html' title='vec_map::VecMap'>VecMap</a>&lt;V&gt;",];
+implementors["vec_map"] = ["impl&lt;V&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='vec_map/struct.VecMap.html' title='vec_map::VecMap'>VecMap</a>&lt;V&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ implementors/core/iter/iterator/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["vec_map"] = ["impl&lt;'a,&nbsp;V&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html' title='core::iter::iterator::Iterator'>Iterator</a> for <a class='struct' href='vec_map/struct.Iter.html' title='vec_map::Iter'>Iter</a>&lt;'a,&nbsp;V&gt;","impl&lt;'a,&nbsp;V&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html' title='core::iter::iterator::Iterator'>Iterator</a> for <a class='struct' href='vec_map/struct.IterMut.html' title='vec_map::IterMut'>IterMut</a>&lt;'a,&nbsp;V&gt;","impl&lt;'a,&nbsp;V&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html' title='core::iter::iterator::Iterator'>Iterator</a> for <a class='struct' href='vec_map/struct.Drain.html' title='vec_map::Drain'>Drain</a>&lt;'a,&nbsp;V&gt;","impl&lt;'a,&nbsp;V&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html' title='core::iter::iterator::Iterator'>Iterator</a> for <a class='struct' href='vec_map/struct.Keys.html' title='vec_map::Keys'>Keys</a>&lt;'a,&nbsp;V&gt;","impl&lt;'a,&nbsp;V&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html' title='core::iter::iterator::Iterator'>Iterator</a> for <a class='struct' href='vec_map/struct.Values.html' title='vec_map::Values'>Values</a>&lt;'a,&nbsp;V&gt;","impl&lt;V&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html' title='core::iter::iterator::Iterator'>Iterator</a> for <a class='struct' href='vec_map/struct.IntoIter.html' title='vec_map::IntoIter'>IntoIter</a>&lt;V&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
